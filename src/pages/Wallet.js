9c62de89d35a36de60e3b508bd94797142a795f7
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-03-17 14:14:43 -0300

    Styles: Começa a estilização da página da carteira

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import Table from '../components/Table';
 import WalletForm from '../components/WalletForm';
+import '../styles/Wallet.css';
 
 class Wallet extends React.Component {
   render() {
     return (
       <div className="wallet-page">
-        <Header />
-        <WalletForm />
+        <div className="upper-scope-container">
+          <Header />
+          <WalletForm />
+        </div>
         <Table />
       </div>
     );
